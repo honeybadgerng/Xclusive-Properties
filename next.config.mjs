2a10d812206462065124cd75c4496b0262a2f052
Onabanji <rajimoshood200482@gmail.com>
2025-07-28 19:37:44 +0100

    stated pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+import nextPwa from "next-pwa";
 
-export default nextConfig;
+const withPwa = nextPwa({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig = {
+  // Your custom Next.js config options (if any)
+};
+
+export default withPwa(nextConfig);
